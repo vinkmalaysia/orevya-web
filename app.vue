75733f70bcd0fa58d6682e6e263b876c98f22d56
kevinlaw91 <kevinlaw91@users.noreply.github.com>
2023-01-20 14:49:05 +0800

    Move global styles to app.vue
    (cherry picked from commit 8eccca4f48c3e5631442fade46d2e4ff981927c5)

---
+++ app.vue
@@ -0,0 +1,37 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage/>
+  </NuxtLayout>
+</template>
+
+<style>
+:root {
+  /* Colors */
+  --color-primary: #895a4a;
+
+  /* Global z-index */
+  --zindex-splash-screen: 999999;
+
+  --zindex-fullscreen-menu: 5000;
+  --zindex-topnav: 5001;
+}
+
+body {
+  --scrollbar-foreground: #6a5841;
+  --scrollbar-background: #f3f4f6;
+  scrollbar-color: var(--scrollbar-foreground) var(--scrollbar-background);
+}
+
+::-webkit-scrollbar {
+  width: 8px;
+  height: 8px;
+}
+
+::-webkit-scrollbar-thumb {
+  background: var(--scrollbar-foreground);
+}
+
+::-webkit-scrollbar-track {
+  background: var(--scrollbar-background);
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -132,36 +132,6 @@
   </div>
 </template>
 
-<style>
-:root {
-  /* Colors */
-  --color-primary: #895a4a;
-
-  /* Global z-index */
-  --zindex-fullscreen-menu: 5000;
-  --zindex-topnav: 5001;
-}
-
-body {
-  --scrollbar-foreground: #6a5841;
-  --scrollbar-background: #f3f4f6;
-  scrollbar-color: var(--scrollbar-foreground) var(--scrollbar-background);
-}
-
-::-webkit-scrollbar {
-  width: 8px;
-  height: 8px;
-}
-
-::-webkit-scrollbar-thumb {
-  background: var(--scrollbar-foreground);
-}
-
-::-webkit-scrollbar-track {
-  background: var(--scrollbar-background);
-}
-</style>
-
 <script setup>
 import AccordionGallerySlide from '~/components/AccordionGallerySlide.vue';
 import AccordionGallerySlideMobile from '~/components/AccordionGallerySlideMobile.vue';
