3e3764892bda671714e5406fa5cb18fdd99b4cce
kevinlaw91 <kevinlaw91@users.noreply.github.com>
2023-02-01 20:02:05 +0800

    Added careers page

--- pages/careers.vue
+++ pages/careers.vue
@@ -1,3 +1,14 @@
 <template>
-
+  <main class="relative w-full pt-14 xl:pt-18 pb-24 bg-[url('/images/interior-1.jpg')] bg-cover">
+    <div class="absolute top-0 w-full h-full bg-white/80"></div>
+    <article class="relative font-Jost">
+      <h1 class="font-bold uppercase text-center my-24 leading-none"><span class="text-transparent bg-clip-text text-[3rem] bg-gradient-to-b from-[#252C2A] to-[#895a4a]">Career</span><br /><span class="text-transparent bg-clip-text text-[2rem] bg-gradient-to-b from-[#252C2A] to-[#895a4a]">Opportunity</span></h1>
+      <div class="flex justify-center my-36">
+        <div class="text-center bg-[var(--color-secondary)] text-white shadow-xl rounded-xl text-xl p-4 py-16 sm:p-16 lg:p-24">
+          <p>You may send your resume via email:</p>
+          <p class="font-medium text-[#D2B48C]">contact@orevya.com</p>
+        </div>
+      </div>
+    </article>
+  </main>
 </template>
