cd28d10d177e0f3b89ccb354988a6d3a9a817ed2
kevinlaw91 <kevinlaw91@users.noreply.github.com>
2023-02-06 20:34:26 +0800

    Update careers page

--- pages/careers.vue
+++ pages/careers.vue
@@ -1,10 +1,10 @@
 <template>
-  <main class="relative w-full pt-14 xl:pt-18 pb-24 bg-[url('/images/interior-1.jpg')] bg-cover">
-    <div class="absolute top-0 w-full h-full bg-white/80"></div>
-    <article class="relative font-Jost">
-      <h1 class="font-bold uppercase text-center my-24 leading-none"><span class="text-transparent bg-clip-text text-[3rem] bg-gradient-to-b from-[#252C2A] to-[#895a4a]">Career</span><br /><span class="text-transparent bg-clip-text text-[2rem] bg-gradient-to-b from-[#252C2A] to-[#895a4a]">Opportunity</span></h1>
-      <div class="flex justify-center my-36">
-        <div class="text-center bg-[var(--color-secondary)] text-white shadow-xl rounded-xl text-xl p-4 py-16 sm:p-16 lg:p-24">
+  <main class="relative w-full pt-14 xl:pt-18 pb-20 bg-neutral-200">
+    <div class="absolute top-0 w-full h-full bg-[url('/images/abstract-tile.png')]"></div>
+    <article class="py-12 relative font-Jost">
+      <h1 class="text-2xl lg:text-[4rem] xl:text-[6rem] text-[var(--color-secondary)] lg:text-neutral-600 font-bold uppercase text-center leading-none py-8 md:py-12">Careers</h1>
+      <div class="flex justify-center">
+        <div class="text-center bg-[var(--color-secondary)] text-white text-xl px-12 py-16 sm:p-16 lg:p-24 border-b-[#b77e4d] border-b-8">
           <p>You may send your resume via email:</p>
           <p class="font-medium text-[#D2B48C]">contact@orevya.com</p>
         </div>
