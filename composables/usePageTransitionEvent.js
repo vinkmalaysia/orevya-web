2fe833dcb99aac104de3dda432d33f52b8d471c4
kevinlaw91 <kevinlaw91@users.noreply.github.com>
2023-02-21 03:38:10 +0800

    Add jsdoc to usePageTransitionEvent

--- composables/usePageTransitionEvent.js
+++ composables/usePageTransitionEvent.js
@@ -1,6 +1,11 @@
 import { useEventListener } from '@vueuse/core';
 import { event } from '~/plugins/pagetransition';
 
+/**
+ * Create an event listener to subscribe to page transition finished event
+ * @param listener
+ * @returns {function} Function to clean up the event listener
+ */
 export default function (listener) {
   return useEventListener(window, event.type, listener);
 }
