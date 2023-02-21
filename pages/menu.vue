3c64b2929e10aab55e4876d31bfc997a5eb057d2
kevinlaw91 <kevinlaw91@users.noreply.github.com>
2023-02-22 03:11:02 +0800

    Update menu page

--- pages/menu.vue
+++ pages/menu.vue
@@ -8,17 +8,17 @@
     </div>
     <section>
       <div class="text-center mt-4 mb-12">
-        <h1 class="inline-block px-4 py-1 font-Jost border-y border-[#8a7d4d] text-2xl xl:text-4xl text-[#8a7d4d] font-medium xl:font-normal uppercase">Our Menu</h1>
+        <h1 class="inline-block px-4 py-1 font-Jost border-y border-[#8a7d4d] text-2xl xl:text-4xl text-white/80 font-medium xl:font-normal uppercase">Our Menu</h1>
       </div>
       <section class="flex flex-col 2xl:flex-row justify-center">
         <section class="menu-section">
           <h2 class="menu-heading" v-motion :initial="{ opacity: 0, y: 80 }" :visibleOnce="{ opacity: 1, y: 0, transition: { type: 'easeIn', duration: 600 }}">Appetizer</h2>
-          <div class="menu-decor my-6">
+          <div class="menu-decor my-6" v-motion :initial="{ opacity: 0, y: 40 }" :visibleOnce="{ opacity: 1, y: 0, transition: { type: 'linear', duration: 600, delay: 50 }}">
             <svg viewBox="0 0 102.34 4.84">
               <path d="M99.58,4.84c-.06,0-.11,0-.17,0-2.22-.13-3.94-1.95-3.99-4.13H6.92c-.06,2.18-1.78,3.99-3.99,4.13C1.41,4.92,.1,3.76,0,2.24-.06,1.23,.72,.36,1.73,.3c.34-.02,.67,.09,.92,.32,.25,.23,.41,.54,.43,.87,.01,.19-.14,.36-.33,.37-.19,.01-.36-.14-.37-.33,0-.15-.08-.29-.19-.39-.11-.1-.26-.15-.41-.14-.63,.04-1.1,.58-1.07,1.2,.07,1.13,1.05,2,2.18,1.93,1.95-.12,3.45-1.8,3.33-3.76l-.02-.37H96.15l-.02,.37c-.12,1.95,1.37,3.64,3.33,3.76,1.13,.07,2.11-.8,2.18-1.93,.04-.63-.44-1.17-1.07-1.2-.15,0-.3,.04-.41,.14-.11,.1-.18,.24-.19,.39-.01,.19-.18,.34-.37,.33-.19-.01-.34-.18-.33-.37,.02-.34,.17-.65,.43-.87,.25-.23,.58-.34,.92-.32,1.01,.06,1.79,.94,1.73,1.95-.09,1.46-1.31,2.59-2.75,2.59Z"/>
             </svg>
           </div>
-          <ul class="menu-list" v-motion :initial="{ opacity: 0, y: 200 }" :visibleOnce="{ opacity: 1, y: 0, transition: { type: 'easeIn', duration: 600 } }">
+          <ul class="menu-list" v-motion :initial="{ opacity: 0, y: 200 }" :visibleOnce="{ opacity: 1, y: 0, transition: { type: 'easeIn', duration: 600, delay: 100 } }">
             <li>Crab Cake</li>
             <li>Hokkaido Scallops</li>
             <li>Baked Portobello with Shrimp</li>
@@ -31,7 +31,7 @@
         </section>
         <section class="menu-section">
           <h2 class="menu-heading" v-motion :initial="{ opacity: 0, y: 80 }" :visibleOnce="{ opacity: 1, y: 0, transition: { type: 'easeIn', duration: 600, delay: 100 }}">Main</h2>
-          <div class="menu-decor my-6">
+          <div class="menu-decor my-6" v-motion :initial="{ opacity: 0, y: 40 }" :visibleOnce="{ opacity: 1, y: 0, transition: { type: 'linear', duration: 600, delay: 150 }}">
             <svg class="w-[150px] mx-auto" viewBox="0 0 102.34 4.84">
               <path d="M99.58,4.84c-.06,0-.11,0-.17,0-2.22-.13-3.94-1.95-3.99-4.13H6.92c-.06,2.18-1.78,3.99-3.99,4.13C1.41,4.92,.1,3.76,0,2.24-.06,1.23,.72,.36,1.73,.3c.34-.02,.67,.09,.92,.32,.25,.23,.41,.54,.43,.87,.01,.19-.14,.36-.33,.37-.19,.01-.36-.14-.37-.33,0-.15-.08-.29-.19-.39-.11-.1-.26-.15-.41-.14-.63,.04-1.1,.58-1.07,1.2,.07,1.13,1.05,2,2.18,1.93,1.95-.12,3.45-1.8,3.33-3.76l-.02-.37H96.15l-.02,.37c-.12,1.95,1.37,3.64,3.33,3.76,1.13,.07,2.11-.8,2.18-1.93,.04-.63-.44-1.17-1.07-1.2-.15,0-.3,.04-.41,.14-.11,.1-.18,.24-.19,.39-.01,.19-.18,.34-.37,.33-.19-.01-.34-.18-.33-.37,.02-.34,.17-.65,.43-.87,.25-.23,.58-.34,.92-.32,1.01,.06,1.79,.94,1.73,1.95-.09,1.46-1.31,2.59-2.75,2.59Z"/>
             </svg>
@@ -52,7 +52,7 @@
         </section>
         <section class="menu-section">
           <h2 class="menu-heading" v-motion :initial="{ opacity: 0, y: 80 }" :visibleOnce="{ opacity: 1, y: 0, transition: { type: 'easeIn', duration: 600, delay: 300 }}">Dessert</h2>
-          <div class="menu-decor my-6">
+          <div class="menu-decor my-6" v-motion :initial="{ opacity: 0, y: 40 }" :visibleOnce="{ opacity: 1, y: 0, transition: { type: 'linear', duration: 600, delay: 350 }}">
             <svg class="w-[150px] mx-auto" viewBox="0 0 102.34 4.84">
               <path d="M99.58,4.84c-.06,0-.11,0-.17,0-2.22-.13-3.94-1.95-3.99-4.13H6.92c-.06,2.18-1.78,3.99-3.99,4.13C1.41,4.92,.1,3.76,0,2.24-.06,1.23,.72,.36,1.73,.3c.34-.02,.67,.09,.92,.32,.25,.23,.41,.54,.43,.87,.01,.19-.14,.36-.33,.37-.19,.01-.36-.14-.37-.33,0-.15-.08-.29-.19-.39-.11-.1-.26-.15-.41-.14-.63,.04-1.1,.58-1.07,1.2,.07,1.13,1.05,2,2.18,1.93,1.95-.12,3.45-1.8,3.33-3.76l-.02-.37H96.15l-.02,.37c-.12,1.95,1.37,3.64,3.33,3.76,1.13,.07,2.11-.8,2.18-1.93,.04-.63-.44-1.17-1.07-1.2-.15,0-.3,.04-.41,.14-.11,.1-.18,.24-.19,.39-.01,.19-.18,.34-.37,.33-.19-.01-.34-.18-.33-.37,.02-.34,.17-.65,.43-.87,.25-.23,.58-.34,.92-.32,1.01,.06,1.79,.94,1.73,1.95-.09,1.46-1.31,2.59-2.75,2.59Z"/>
             </svg>
